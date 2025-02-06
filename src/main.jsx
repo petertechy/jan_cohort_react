8551902a294133c365e34bb8990b33e11562eedc
petertechy <petertechy01@gmail.com>
2025-02-06 16:57:25 +0100

    Modify main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 // import "../node_modules/bootstrap/dist/css/bootstrap.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.js"
-import { BrowserRouter } from 'react-router-dom'
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import counterReducer from "./Redux/counterSlice.js";
 
-createRoot(document.getElementById('root')).render(
+export const store = configureStore({
+  reducer: {counterReducer},
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter> 
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);

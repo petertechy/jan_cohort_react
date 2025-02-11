c62994207f281430999e45998207717c55863b27
petertechy <petertechy01@gmail.com>
2025-02-11 12:08:03 +0100

    contact updated

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, increaseByNumber } from "../Redux/counterSlice";
+import { increment, decrement, increaseByNumber, saveStudent } from "../Redux/counterSlice";
 
 const Contact = () => {
   const [num, setnum] = useState(0);
   const [myName, setmyName] = useState("")
+  const [myAge, setmyAge] = useState(0)
   const count = useSelector((state) => state.counterReducer.count);
-  console.log(count);
+  // console.log(count);
   let dispatch = useDispatch();
   // const count = useSelector(function (state){
   // return state;
@@ -31,14 +32,19 @@ const Contact = () => {
       <input
         type="text"
         placeholder="enter your name"
-        onChange={(e) => setmyName()}
+        onChange={(e) => setmyName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="enter your age"
+        onChange={(e) => setmyAge(e.target.value)}
       />
       <button onClick={() => dispatch(increment())}>Increase Number</button>
       <button onClick={() => dispatch(decrement())}>Decrease Number</button>
       <button onClick={() => dispatch(increaseByNumber(num))}>
         Increase By Number
       </button>
-      <button onClick={() => dispatch()}>
+      <button onClick={() => dispatch(saveStudent({myName, myAge}))}>
         Save Student
       </button>
 

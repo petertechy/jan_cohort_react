0791a2ab2f1588a198f5a59fc85be9ae14f91563
petertechy <petertechy01@gmail.com>
2025-02-17 17:10:22 +0100

    Updated Reference.jsx

--- src/Pages/Reference.jsx
+++ src/Pages/Reference.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 const Reference = () => {
-    const [number, setnumber] = useState(0)
-    let myNumber = useRef(0)
+    // const [number, setnumber] = useState(0)
+    let myNumber = useRef(9)
     let myHeader = useRef("")
     useEffect(() => {
-        setnumber(3)
+        // setnumber(3)
         myNumber.current = 9
         console.log(myNumber.current)
     })
 
     const changeHeader = ()=>{
-        myHeader.current.innerText = "Hello Martha"
+        myHeader.current.innerText = "Good morning Everybody"
         myHeader.current.style.color = "green"
         myHeader.current.style.fontSize = "50px"
         console.log(myHeader.current.innerText)
@@ -23,7 +23,7 @@ const Reference = () => {
         <h1>Use Reference Hook</h1>
         <h1 ref={myHeader}>Hello World</h1>
         <button onClick={changeHeader}>Change Name</button>
-        <h1>{number}</h1>
+        <h1>{myNumber.current}</h1>
     </>
   )
 }

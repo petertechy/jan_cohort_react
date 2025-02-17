7b5e55541a23aebd7ebde2b3d7e1720b96eec980
petertechy <petertechy01@gmail.com>
2025-02-17 17:10:19 +0100

    Updated Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
+  let navigate = useNavigate()
+  let token = localStorage.token
+  const handleSignOut =()=>{
+    localStorage.removeItem("token")
+    console.log(localStorage)
+    navigate('/signin')
+  }
+
+  useEffect(() => {
+    getdashboard()
+  }, [])
+  
+  let url = "http://localhost:5500/user/getdashboard"
+
+
+  
+  const getdashboard = () =>{
+    axios.get(url,{
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type":  "application/json",
+        "Accept": "application/json"
+  
+      }
+    })
+    .then((response)=>{
+      console.log(response)
+      if(!response.data.status){
+        localStorage.removeItem(token)
+        navigate('/signin')
+      }
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+
   return (
     <>
         <h1>This is my Profile</h1>
+
+        <button onClick={handleSignOut}>Sign Out</button>
     </>
   )
 }
